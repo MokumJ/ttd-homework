d63bb3f042380c11a12566fe0a280e4b78483d5f
MokumJ <jesse.vanderheijden@gmail.com>
2017-11-24 22:43:49 +0100

    some + signs working test B needs fix

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -15,19 +15,27 @@ class Codebreaker
 
     def guess(input)
         if input.length != 4
-      output.puts "Try guessing a number with four digits"
+          output.puts "Try guessing a number with four digits"
       end
         hint = ''
         (0..3).each do |array|
           if matching_number(input, array)
-            output.puts hint << '-'
+            @output.puts hint << '-'
           end
-          end
-      output.puts hint
         end
+        (0..3).each do |array|
+        if match(input, array)
+            @output.puts hint << '+'
+        end
+      end
+      @output.puts hint
+  end
 
     def matching_number(input, array)
-      @secret_number.include?(input[array])
-  end
+      @secret_number.include?(input[array]) && !match(input, array)
+    end
+    def match(input, array)
+      input[array] == @secret_number[array]
+    end
 end
 end
