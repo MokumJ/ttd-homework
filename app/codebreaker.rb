1bb36b951b833f7d5837f794fc7a088fe9c4f47a
MokumJ <jesse.vanderheijden@gmail.com>
2017-11-24 22:27:46 +0100

    number match working output -

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -16,11 +16,18 @@ class Codebreaker
     def guess(input)
         if input.length != 4
       output.puts "Try guessing a number with four digits"
-
       end
-        if input != @secret_number
-            output.puts ''
+        hint = ''
+        (0..3).each do |array|
+          if matching_number(input, array)
+            output.puts hint << '-'
+          end
+          end
+      output.puts hint
         end
-    end
+
+    def matching_number(input, array)
+      @secret_number.include?(input[array])
   end
+end
 end
