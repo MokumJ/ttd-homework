21817bbab617b88e570623244e4cd959db43db22
MokumJ <jesse.vanderheijden@gmail.com>
2017-11-25 17:58:58 +0100

    refactoring

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -14,32 +14,25 @@ class Codebreaker
     end
 
     def guess(input)
-      input.length!=4 ? @output.puts("Try guessing a number with four digits")
-       : @output.puts('+'*hint_plus(input) + '-'*hint_min(input))
-   end
-   def hint_plus(input)
-      hint_plus = 0
+     input.length!=4 ? @output.puts("Try guessing a number with four digits")
+       : hint(input)
+     end
+     def hint(input)
+      hint1, hint2 = '' , ''
       (0..3).each do |m|
-       if match(input, m)
-      hint_plus += 1
-      end
-    end
-      hint_plus
-  end
-  def hint_min(input)
-          hint_min = 0
-         (0..3).each do |m|
-    if matching_number(input, m)
-        hint_min += 1
-        end
-      end
-    hint_min
-  end
-  def matching_number(input, m)
+         if match(input, m)
+            hint1 << '+'
+         elsif matching_number(input, m)
+            hint2 << '-'
+         end
+       end
+       @output.puts hint1 + hint2
+     end
+     def matching_number(input, m)
       @secret_number.include?(input[m]) && !match(input, m)
     end
-  def match(input, m)
+    def match(input, m)
     input[m] == @secret_number[m]
+    end
   end
-end
 end
