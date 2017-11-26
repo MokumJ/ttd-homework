84464824a8d0f087a4ceceede1d63506ea8c6888
MokumJ <jesse.vanderheijden@gmail.com>
2017-11-26 14:31:51 +0100

    implement for loop, 28 lines

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -1,38 +1,28 @@
 class Codebreaker
   class Game
-    attr_reader :output
+  attr_reader :output
 
-    def initialize(output)
+  def initialize(output)
       @output = output
-    end
-
-    def start(secret_number)
+  end
+  def start(secret_number)
       @secret_number = secret_number
-
       @output.puts "Welcome to Codebreaker"
       @output.puts "Enter guess:"
+   end
+   def guess(input)
+     input.length!=4 ? @output.puts("Try guessing a number with four digits") : hint(input)
     end
-
-    def guess(input)
-     input.length!=4 ? @output.puts("Try guessing a number with four digits")
-       : hint(input)
-     end
-     def hint(input)
-      hint1, hint2 = '' , ''
-      (0..3).each do |m|
-         if match(input, m)
-            hint1 << '+'
-         elsif matching_number(input, m)
-            hint2 << '-'
-         end
-       end
-       @output.puts hint1 + hint2
-     end
-     def matching_number(input, m)
-      @secret_number.include?(input[m]) && !match(input, m)
-    end
-    def match(input, m)
-    input[m] == @secret_number[m]
+    def hint(input)
+       hint = ''
+      for m in (0..3)
+          if   input[m] == @secret_number[m]
+            hint << '+'
+          elsif @secret_number.include? input[m]
+            hint << '-'
+          end
+        end
+        @output.puts hint.split('').sort.join
     end
   end
 end
